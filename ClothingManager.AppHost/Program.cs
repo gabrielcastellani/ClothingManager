7daf08baaf46b6fda3ec20d960c508ccc91cd5b6
Gabriel Castellani de Oliveira <gabriel.castellani@remotatec.com.br>
2025-05-01 11:35:36 -0300

    Adding packages and initializing infrastructure services (Redis, RabbitMQ and Postgres)

--- ClothingManager.AppHost/Program.cs
+++ ClothingManager.AppHost/Program.cs
@@ -1,13 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("cache");
+var redisCache = builder.AddRedis("redis-cache");
+var rabbitMq = builder.AddRabbitMQ("eventbus-rabbitMQ");
+var postgresSql = builder.AddPostgres("database-postgresSQL");
 
 var apiService = builder.AddProject<Projects.ClothingManager_ApiService>("apiservice");
 
 builder.AddProject<Projects.ClothingManager_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(cache)
-    .WaitFor(cache)
+    .WithReference(redisCache)
+    .WaitFor(redisCache)
+    .WithReference(rabbitMq)
+    .WaitFor(rabbitMq)
+    .WithReference(postgresSql)
+    .WaitFor(postgresSql)
     .WithReference(apiService)
     .WaitFor(apiService);
 
